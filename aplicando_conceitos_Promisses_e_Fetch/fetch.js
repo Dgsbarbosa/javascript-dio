b462504c94cd61c9819f3b38ec454062b87598a3
Dgsbarbosa <guinho.cq@gmail.com>
2022-02-25 18:23:57 -0300

    continue

---
+++ aplicando_conceitos_Promisses_e_Fetch/fetch.js
@@ -0,0 +1,16 @@
+fetch('http//localhost8080/data.json')
+    .then (responseStream =>{
+        if(responseStream.status ===200){
+            return responseStream.json();
+
+        }
+        else{
+            throw new Error('Request error')
+        }
+    })
+    .then(data =>{
+        console.log(data);
+    })
+    .catch(err => { 
+        console.log('Error: ', err);
+    })
